9a0002f2f2361935e5a04c40b7f89d2c4af335c6
Bruno Rodrigues Marques <brunomarque2002@gmail.com>
2024-09-16 20:58:16 -0300

    Tailwind CSS instalado

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './**/*.html', // Procura em todos os arquivos HTML
+    './src/**/*.{js,ts,jsx,tsx}' // Procura em arquivos JavaScript e TypeScript na pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
